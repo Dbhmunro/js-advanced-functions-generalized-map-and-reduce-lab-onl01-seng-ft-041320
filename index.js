1f23537107e4efe5ce7414487f976a6966eb18e1
dbhmunro <dbhmunro@gmail.com>
2020-07-29 23:41:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+function map(arr, func) {
+    const newArr = []
+    for ( let i=0; i<arr.length; i++){
+        newArr.push(func(arr[i]))
+    }
+    return newArr
+}
+
+function reduce(arr, func, start) {
+    if (!!start) {
+        for ( let i=0; i<arr.length; i++){
+            start = func(arr[i], start)
+        }
+        return start
+    } else {
+        start = arr[0]
+        for ( let i=1; i<arr.length; i++){
+            start = func(arr[i], start)
+        }
+        return start
+    }
+}
